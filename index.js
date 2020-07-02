e4fd66ac780144127169a16ad394bc943f8d6a7b
KPrerana4 <prerana.kannegundla@thoughtworks.com>
2020-07-02 08:53:27 +0530

    added code for sub to-do operations

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
 
-var toDos = []
+var toDos = [];
+var subToDos = [];
 const app = express();
 app.use(express.static(path.join(__dirname,"public")));
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -11,7 +12,11 @@ app.post("/add_item",(req,res) => {
     let found = toDos.some((toDo) => { return (toDo.item === req.body.item)});
     if(!found){
         toDos.push(req.body);
+        subToDos.push({"items" : []});
     }
+    console.log("in add main");
+    console.log(toDos);
+    console.log(subToDos);
     res.status(204).send();
 });
 
@@ -21,12 +26,15 @@ app.post("/delete_item",(req,res) => {
     {
         if(toDos[index].item === req.body.editItem)
             {
-                console.log("in if");
                 reqIndex = index;
                 break;
             }
     }
     toDos.splice(reqIndex,1);
+    subToDos.splice(reqIndex,1);
+    console.log("in delete main");
+    console.log(toDos);
+    console.log(subToDos);
     res.status(204).send();
 });
 
@@ -35,9 +43,22 @@ app.post('/update',(req,res) =>{
     let value = req.body.editItem;
     toDos.splice(reqIndex , 1);
     toDos.splice(reqIndex, 0 , {item : value});
+    console.log("in update main");
+    console.log(toDos);
+    console.log(subToDos);
     res.status(204).send();
 });
 
-app.get("/toDoList",(req,res) => res.json(toDos));
+app.post('/update_sub_todo',(req,res) => {
+    let index = req.body.index;
+    subToDos[index].items = req.body.subToDo.split(",");
+    console.log("in update sub todos");
+    console.log(toDos);
+    console.log(subToDos);
+    res.status(204).send();
+});
 
-app.listen(5400, () => console.log("running"));
+app.get("/mainToDos",(req,res) => res.json(toDos));
+app.get("/subToDos",(req,res) => res.json(subToDos));
+
+app.listen(5100, () => console.log("running"));
