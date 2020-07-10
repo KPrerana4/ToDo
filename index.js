db43a5edc793a2bed4afc095c7badb2ad6ce9f4f
KPrerana4 <prerana.kannegundla@thoughtworks.com>
2020-07-10 14:01:15 +0530

    storing data using database

--- index.js
+++ index.js
@@ -1,51 +1,67 @@
 const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
+const mongoClient = require("mongodb").MongoClient;
 
-var toDos = [];
-var subToDos = [];
+var db;
+
 const app = express();
 app.use(express.static(path.join(__dirname,"public")));
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: true }));
+mongoClient.connect("mongodb://localhost:27017/sample",{useUnifiedTopology: true },(err,client) => {
+    if(err) throw err;
+    db = client.db("sample");
+});
 
+var count = 0;
 app.post("/add_item",(req,res) => {
-    let found = toDos.some((toDo) => { return (toDo.item === req.body.item)});
-    if(!found){
-        toDos.push({item : req.body.item});
-        subToDos.push({"items" : []});
+    let found = false;
+    db.collection("ToDos").findOne({toDo : req.body.item}, (err,result) => {
+        if(err) throw err;
+        if(result != undefined)
+            found = true;
+    });
+    if(found == false)
+    {
+        db.collection("ToDos").insertOne({toDo : req.body.item , subToDos : []});
+        count++;
     }
     res.status(204).send();
 });
 
 app.post("/delete_item",(req,res) => {
-    let reqIndex = -1;
-    for(index = 0; index < toDos.length; index++)
-    {
-        if(toDos[index].item === req.body.editItem)
-            {
-                reqIndex = index;
-                break;
-            }
-    }
-    toDos.splice(reqIndex,1);
-    subToDos.splice(reqIndex,1);
+    db.collection("ToDos").deleteOne({toDo : req.body.editItem});
     res.status(204).send();
 });
 
 app.post('/update',(req,res) =>{
-    let reqIndex = req.body.index;
-    let value = req.body.editItem;
-    toDos.splice(reqIndex , 1, {item : value});
+    db.collection("ToDos").updateOne({toDo : req.body.oldText},{
+        $set:{
+            toDo : req.body.editItem
+        }
+    });
     res.status(204).send();
 });
 
 app.post('/update_sub_todo',(req,res) => {
-    let index = req.body.index;
-    subToDos[index].items = req.body.subToDo.split(",");
+    db.collection("ToDos").updateOne({toDo : req.body.mainToDo},{
+        $set : {
+            subToDos : req.body.subToDo.split(",")
+        }
+    });
+
     res.status(204).send();
 });
 
-app.get("/mainToDos",(req,res) => res.json(toDos));
-app.get("/subToDos",(req,res) => res.json(subToDos));
+app.get("/ToDos",(req,res) => {
+    var result;
+    db.collection("ToDos").find().toArray((err,data) => {
+        if(err) throw err;
+        result = data;
+        console.log(data);
+    });
+    console.log(result);
+    res.json(result);
+});
 
-app.listen(5100, () => console.log("running"));
+app.listen(5600, () => console.log("running"));
